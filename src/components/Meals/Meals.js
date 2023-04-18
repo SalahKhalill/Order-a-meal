68fd7efcb2b487612d2e5f7e2e583e01f33135ad
Salah Khalil <108340266+SalahKhalill@users.noreply.github.com>
2023-04-18 21:11:39 +0300

    Add files via upload

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,11 @@
+import MealsSummary from './MealsSummary'
+import AvailableMeals from './AvailableMeals'
+function Meals(props) {
+    return (
+        <>
+            <MealsSummary />
+            <AvailableMeals />
+        </>
+    )
+}
+export default Meals
